571baf5d6912e66f04a4f235f9787093f21093d4
Mike Pettypiece <mike@pettypiece.ca>
2024-11-01 12:01:06 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Install required packages
+apt-get update -y
+apt-get -y install zsh git
+
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Homeshick
+git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+$HOME/.homesick/repos/homeshick/bin/homeshick clone mtpettyp/dotfiles
