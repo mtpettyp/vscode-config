811178a7e599f94d9648d4ab51bbd902d91e0d26
Mike Pettypiece <mike@pettypiece.ca>
2024-11-05 14:07:20 -0500

    Remove unnecessary check

--- install.sh
+++ install.sh
@@ -11,14 +11,6 @@ fi
 $SUDO apt-get update -y
 $SUDO apt-get -y install zsh git vim
 
-OH_MY_ZSH_DIR="$HOME/.oh-my-zsh"
-
-# Check if the directory exists
-if [ -d "$OH_MY_ZSH_DIR" ]; then
-    # Rename the directory by appending .bak
-    mv "$OH_MY_ZSH_DIR" "$OH_MY_ZSH_DIR.bak"
-fi
-
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
